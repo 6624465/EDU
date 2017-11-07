ae7a18130933d60fca959f0c50d578b16f75bc5f
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-07 15:22:55 +0530

    Registration : maruthi

--- EDU.Web/Controllers/RegistrationController.cs
+++ EDU.Web/Controllers/RegistrationController.cs
@@ -21,22 +21,57 @@ namespace EDU.Web.Controllers
         public ActionResult Registration(int Id)
         {
             Registration registration = dbContext.Registrations.Where(x => x.RegistrationId == Id).FirstOrDefault();
-
+            if (registration == null)
+            {
+                registration = new Registration();
+                registration.RegistrationId = Id;
+            }
             return View(registration);
         }
         [HttpPost]
         public ActionResult Registration(Registration registration)
         {
-            if (registration.RegistrationId != -1)
+            if (registration.RegistrationId == -1)
             {
                 dbContext.Registrations.Add(registration);
                 dbContext.SaveChanges();
             }
             else
             {
-
+                Registration regObj = dbContext.Registrations.Where(x => x.RegistrationId == registration.RegistrationId).FirstOrDefault();
+                if (regObj != null)
+                {
+                    regObj.OracleDataBase = registration.OracleDataBase;
+                    regObj.Payment1 = registration.Payment1;
+                    regObj.Payment2 = registration.Payment2;
+                    regObj.Product = registration.Product;
+                    regObj.StartDate = registration.StartDate;
+                    regObj.TrainerName = registration.TrainerName;
+                    regObj.Email = registration.Email;
+                    regObj.Amount = registration.Amount;
+                    regObj.Balance = registration.Balance;
+                    regObj.CompanyName = registration.CompanyName;
+                    regObj.Contact = registration.Contact;
+                    regObj.CourseName = registration.CourseName;
+                    regObj.VAT = registration.VAT;
+                    regObj.WHT = registration.WHT;
+                    //regObj. = registration.CompanyName;
+                }
+                dbContext.SaveChanges();
             }
-            return View();
+            return RedirectToAction("GetRegistrationList");
+        }
+
+        [HttpPost]
+        public JsonResult DeleteRegistration(int Id)
+        {
+            Registration regObj = dbContext.Registrations.Where(x => x.RegistrationId == Id).FirstOrDefault();
+            if (regObj != null)
+            {
+                dbContext.Registrations.Remove(regObj);
+                dbContext.SaveChanges();
+            }
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
     }
 }
