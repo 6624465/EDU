186f8128a7f1e10f8cb94e1e2fff9ac5f2d38e29
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-07 14:24:08 +0530

    Registration : maruthi

---
+++ EDU.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using EDU.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDU.Web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        EducationEntities dbContext = new EducationEntities();
+
+        [HttpGet]
+        public ActionResult GetRegistrationList()
+        {
+            List<Registration> registrationList = dbContext.Registrations.ToList();
+            return View(registrationList);
+        }
+
+        public ActionResult Registration(int Id)
+        {
+            Registration registration = dbContext.Registrations.Where(x => x.RegistrationId == Id).FirstOrDefault();
+
+            return View(registration);
+        }
+        [HttpPost]
+        public ActionResult Registration(Registration registration)
+        {
+            if (registration.RegistrationId != -1)
+            {
+                dbContext.Registrations.Add(registration);
+                dbContext.SaveChanges();
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+    }
+}
