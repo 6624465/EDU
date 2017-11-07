7472716e5f28e86087c35178bf4d30bd43fe47c1
Naresh K <Naresh@sulabs.tech>
2017-11-07 12:43:22 +0530

    changes in master controller

--- EDU.Web/Controllers/MasterController.cs
+++ EDU.Web/Controllers/MasterController.cs
@@ -416,7 +416,7 @@ namespace EDU.Web.Controllers
         }
 
 
-        [HttpDelete]
+        [HttpPost]
         public ActionResult DeleteCourseSalesMaster(int? Id)
         {
             var result = new CourseSalesMasterBO().DeleteCourseSalesMaster(new CourseSalesMaster { Id = Id.Value });
