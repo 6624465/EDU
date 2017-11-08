e659d1d08fd023a20961fabaa6b3728533349ffb
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-08 12:05:52 +0530

    Customer Payments : maruthi

---
+++ EDU.Web/Controllers/CustomerPaymentController.cs
@@ -0,0 +1,24 @@
+﻿using EDU.Web.ViewModels.CustomerPayments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDU.Web.Controllers
+{
+    public class CustomerPaymentController : Controller
+    {
+        // GET: CustomerPayment
+        public ActionResult Payment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Payment(CustomerPaymentVM customerPayment)
+        {
+            return View();
+        }
+
+    }
+}
--- EDU.Web/ViewModels/Trainer/TrainerVM.cs
+++ EDU.Web/ViewModels/Trainer/TrainerVM.cs
@@ -13,4 +13,17 @@ namespace EDU.Web.ViewModels.Trainer
         public IEnumerable<Country> countryList { get; set; }
 
     }
+
+    public class TrainerInformationVM {
+        public int TrianerId { get; set; }
+        public string Technology { get; set; }
+        public string Country { get; set; }
+        public HttpPostedFileBase Profile { get; set; }
+        public Nullable<decimal> TrainerRate { get; set; }
+        public string VendorName { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+        public string Remarks { get; set; }
+        public string TrainerName { get; set; }
+    }
 }
