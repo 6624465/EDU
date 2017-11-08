f04c43a36085abded174759b919b2bd3ff373c46
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-08 12:35:03 +0530

    Customer payments : maruthi

--- EDU.Web/Controllers/CustomerPaymentController.cs
+++ EDU.Web/Controllers/CustomerPaymentController.cs
@@ -1,4 +1,5 @@
-﻿using EDU.Web.ViewModels.CustomerPayments;
+﻿using EDU.Web.Models;
+using EDU.Web.ViewModels.CustomerPayments;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,101 @@ namespace EDU.Web.Controllers
 {
     public class CustomerPaymentController : Controller
     {
+        EducationEntities dbContext = new EducationEntities();
         // GET: CustomerPayment
-        public ActionResult Payment()
+        public ActionResult Payment(int customerId = -1)
         {
-            return View();
+            CustomerPayment customerPayment = dbContext.CustomerPayments.
+                    Where(x => x.CustomerId == customerId).FirstOrDefault();
+
+            if (customerPayment == null)
+            {
+                CustomerPaymentVM customerPaymentVM = new CustomerPaymentVM();
+                customerPaymentVM.CustomerId = customerId;
+
+                return View(customerPaymentVM);
+            }
+            else
+            {
+
+                CustomerPaymentVM customerPaymentVM = new CustomerPaymentVM();
+
+                customerPaymentVM.BalanceAmount = customerPayment.BalanceAmount;
+                customerPaymentVM.CourseName = customerPayment.CourseName;
+                customerPaymentVM.CustomerName = customerPayment.CustomerName;
+                customerPaymentVM.DueDate = customerPayment.DueDate;
+                customerPaymentVM.InvoiceValue = customerPayment.InvoiceValue;
+                customerPaymentVM.OrderId = customerPayment.OrderId;
+                customerPaymentVM.OtherRecievables = customerPayment.OtherRecievables;
+                customerPaymentVM.PaidAmount = customerPayment.PaidAmount;
+                customerPaymentVM.RecieptDate = customerPayment.RecieptDate;
+                return View(customerPaymentVM);
+            }
+
+
+
         }
         [HttpPost]
-        public ActionResult Payment(CustomerPaymentVM customerPayment)
+        public ActionResult Payment(CustomerPaymentVM customerPaymentVM)
         {
-            return View();
+            if (customerPaymentVM.CustomerId == -1)
+            {
+                CustomerPayment customerPayment = new CustomerPayment();
+                customerPayment.BalanceAmount = customerPaymentVM.BalanceAmount;
+                customerPayment.CourseName = customerPaymentVM.CourseName;
+                customerPayment.CustomerName = customerPaymentVM.CustomerName;
+                customerPayment.DueDate = customerPaymentVM.DueDate;
+                customerPayment.InvoiceValue = customerPaymentVM.InvoiceValue;
+                customerPayment.OrderId = customerPaymentVM.OrderId;
+                customerPayment.OtherRecievables = customerPaymentVM.OtherRecievables;
+                customerPayment.PaidAmount = customerPaymentVM.PaidAmount;
+                customerPayment.RecieptDate = customerPaymentVM.RecieptDate;
+                customerPayment.ReferenceDocument = customerPaymentVM.ReferenceDocument.FileName;
+
+                dbContext.CustomerPayments.Add(customerPayment);
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                CustomerPayment customerPayment = dbContext.CustomerPayments.
+                    Where(x => x.CustomerId == customerPaymentVM.CustomerId).FirstOrDefault();
+
+                customerPayment.BalanceAmount = customerPaymentVM.BalanceAmount;
+                customerPayment.CourseName = customerPaymentVM.CourseName;
+                customerPayment.CustomerName = customerPaymentVM.CustomerName;
+                customerPayment.DueDate = customerPaymentVM.DueDate;
+                customerPayment.InvoiceValue = customerPaymentVM.InvoiceValue;
+                customerPayment.OrderId = customerPaymentVM.OrderId;
+                customerPayment.OtherRecievables = customerPaymentVM.OtherRecievables;
+                customerPayment.PaidAmount = customerPaymentVM.PaidAmount;
+                customerPayment.RecieptDate = customerPaymentVM.RecieptDate;
+                customerPayment.ReferenceDocument = customerPaymentVM.ReferenceDocument.FileName;
+
+                dbContext.SaveChanges();
+            }
+            return View("GetCustomerPayments");
         }
+
+        public ActionResult CustomerPaymentList()
+        {
+            List<CustomerPayment> customerPayment = dbContext.CustomerPayments.ToList();
+            return View(customerPayment);
+        }
+
+
+        public JsonResult DeleteCustomerPayment(int customerId)
+        {
+            CustomerPayment customerPayment = dbContext.CustomerPayments.
+                      Where(x => x.CustomerId == customerId).FirstOrDefault();
+
+            if (customerPayment != null)
+            {
+                dbContext.CustomerPayments.Remove(customerPayment);
+            }
+
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
 
     }
 }
